87c1788ff774fade41793fbafd1ca97c20538ad7
UGMWS <UGMWS@Candyman>
2015-05-13 19:38:30 -0600

    1.3

--- Tarea1_Unidad3/src/Problema6/P6.java
+++ Tarea1_Unidad3/src/Problema6/P6.java
@@ -5,10 +5,24 @@
  */
 package Problema6;
 
+import java.awt.BorderLayout;
+import java.util.Scanner;
+
 /**
  *
  * @author UGMWS
  */
 public class P6 {
     
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero1;
+        System.out.print("Escribe el numero hasta cual deseas que se imprima: ");
+        numero1 = entrada.nextInt();
+        System.out.println("===================================================");
+         for (int i = 0; i <= numero1; i++) {
+                System.out.println(i);
+        }
+        
+    }
 }
--- Tarea1_Unidad3/src/Problema7/P7.java
+++ Tarea1_Unidad3/src/Problema7/P7.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package Problema7;
-
+import java.awt.BorderLayout;
+import java.util.Scanner;
 /**
  *
  * @author UGMWS
  */
 public class P7 {
-    
-}
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        String frases;
+        String opcion ="S";
+        int cont=1;
+      
+        while(opcion.equals("S")){
+            System.out.print("Escribe la frase: ");
+            frases = entrada.nextLine();
+            System.out.println("=============================================");
+            System.out.println(cont + ".-" +frases);
+            cont = cont +1;
+            System.out.print("Deseas introducir mas frases?(S/N):");
+            opcion = entrada.nextLine();
+        }
+        
+        System.out.println("Cantidad de frases: "+cont);
+        
+    }
+    }
+
